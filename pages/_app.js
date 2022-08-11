b0e0f023800e626caf37ec17cc052895193bba5c
Arjun <arjun.varshney1423@gmail.com>
2022-08-11 20:53:02 +0530

    Initial commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,51 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import LoadingBar from "react-top-loading-bar";
+import Navbar from "../components/Navbar";
+import { useState, useEffect } from "react";
+import { useRouter } from "next/router";
+import { app, database } from "../firebaseConfig";
+import {
+  collection,
+  addDoc,
+  getDocs,
+  updateDoc,
+  deleteDoc,
+} from "firebase/firestore";
+const mentors = collection(database, "mentors");
+const mentees = collection(database, "mentees");
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const [progress, setprogress] = useState(0);
+  const router = useRouter();
+  useEffect(() => {
+    router.events.on("routeChangeStart", () => {
+      setprogress(40);
+    });
+    router.events.on("routeChangeComplete", () => {
+      setprogress(100);
+    });
+  }, [router.query]);
+  return (
+    <>
+      <LoadingBar
+        color="#4B0082"
+        waitingTime={400}
+        progress={progress}
+        onLoaderFinished={() => setprogress(0)}
+      />
+      <Navbar />
+      <Component
+        app={app}
+        mentors={mentors}
+        mentees={mentees}
+        addDoc={addDoc}
+        getDocs={getDocs}
+        updateDoc={updateDoc}
+        deleteDoc={deleteDoc}
+        {...pageProps}
+      />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
