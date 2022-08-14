949d4a8e271cbff5ee7c689ba01a24ef6c96adcd
Arjun <arjun.varshney1423@gmail.com>
2022-08-14 23:58:52 +0530

    test succeeded i guess

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,8 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-import { getAuth } from "firebase/auth";
-import { initializeFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -19,10 +17,5 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-const database = initializeFirestore(app, {
-  experimentalForceLongPolling: true,
-});
-export { auth, database };
-export const db = getFirestore(app);
+export const app = initializeApp(firebaseConfig);
+export const database = getFirestore(app);
--- pages/_app.js
+++ pages/_app.js
@@ -4,7 +4,7 @@ import Navbar from "../components/Navbar";
 import { useState, useEffect } from "react";
 import { useRouter } from "next/router";
 import { GoogleOAuthProvider } from "@react-oauth/google";
-import { database } from "../firebaseConfig";
+import { app, database } from "../firebaseConfig";
 import {
   collection,
   addDoc,
@@ -39,6 +39,7 @@ function MyApp({ Component, pageProps }) {
         clientId={`${process.env.NEXT_PUBLIC_GOOGLE_API_TOKEN}`}
       >
         <Component
+          app={app}
           mentors={mentors}
           mentees={mentees}
           addDoc={addDoc}
