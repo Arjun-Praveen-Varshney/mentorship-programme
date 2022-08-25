79bc29c540a5c3aa5d620f56aa29177e57874305
Maverick-1423 <99124051+Maverick-1423@users.noreply.github.com>
2022-08-26 00:48:44 +0530

    update mentorprofile page

--- pages/mentorprofiles/[slug].js
+++ pages/mentorprofiles/[slug].js
@@ -109,113 +109,135 @@ const Slug = ({ onementor }) => {
                 </div>
                 <div className="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
                   <dt className="text-sm font-medium text-gray-500">
-                    LinkedIn Profile
+                    Company currently working in
                   </dt>
                   <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
-                    {teacher.linkedin}
+                    {teacher.company}
                   </dd>
                 </div>
-                <div className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-                  <dt className="text-sm font-medium text-gray-500">
-                    GitHub Profile
-                  </dt>
-                  <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
-                    {teacher.github}
-                  </dd>
-                </div>
-                <div className="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-                  <dt className="text-sm font-medium text-gray-500">
-                    Portfolio
-                  </dt>
-                  <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
-                    {teacher.portfolio}
-                  </dd>
-                </div>
-                <div className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-                  <dt className="text-sm font-medium text-gray-500">
-                    Personal Website
-                  </dt>
-                  <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
-                    {teacher.personalwebsite}
-                  </dd>
-                </div>
-                <div className="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-                  <dt className="text-sm font-medium text-gray-500">
-                    Attachments
-                  </dt>
-                  <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
-                    <ul
-                      role="list"
-                      className="border border-gray-200 rounded-md divide-y divide-gray-200"
-                    >
-                      <li className="pl-3 pr-4 py-3 flex items-center justify-between text-sm">
-                        <div className="w-0 flex-1 flex items-center">
-                          <svg
-                            className="flex-shrink-0 h-5 w-5 text-gray-400"
-                            xmlns="http://www.w3.org/2000/svg"
-                            viewBox="0 0 20 20"
-                            fill="currentColor"
-                            aria-hidden="true"
-                          >
-                            <path
-                              fillRule="evenodd"
-                              d="M8 4a3 3 0 00-3 3v4a5 5 0 0010 0V7a1 1 0 112 0v4a7 7 0 11-14 0V7a5 5 0 0110 0v4a3 3 0 11-6 0V7a1 1 0 012 0v4a1 1 0 102 0V7a3 3 0 00-3-3z"
-                              clipRule="evenodd"
-                            />
-                          </svg>
-                          <span className="ml-2 flex-1 w-0 truncate">
-                            {" "}
-                            cv.pdf{" "}
-                          </span>
-                        </div>
-                        <div className="ml-4 flex-shrink-0">
-                          <a
-                            href="#"
-                            className="font-medium text-indigo-600 hover:text-indigo-500"
-                          >
-                            {" "}
-                            Download{" "}
-                          </a>
-                        </div>
-                      </li>
-                    </ul>
-                  </dd>
-                </div>
-                <div className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-                  <dt className="text-sm font-medium text-gray-500">Actions</dt>
-                  <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
-                    <ul
-                      role="list"
-                      className="border border-gray-200 rounded-md divide-y divide-gray-200"
-                    >
-                      <li className="pl-3 pr-4 py-3 flex items-center justify-between text-sm">
-                        {user && user.loginemail != teacher.email && (
-                          <div className="p-2 w-full">
-                            <button
-                              onClick={updateDocument}
-                              className="w-full border-2 text-white bg-indigo-500 rounded-md p-2 hover:bg-indigo-600"
+                {teacher.linkedin && (
+                  <div className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                    <dt className="text-sm font-medium text-gray-500">
+                      LinkedIn Profile
+                    </dt>
+                    <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                      {teacher.linkedin}
+                    </dd>
+                  </div>
+                )}
+                {teacher.github && (
+                  <div className="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                    <dt className="text-sm font-medium text-gray-500">
+                      GitHub Profile
+                    </dt>
+                    <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                      {teacher.github}
+                    </dd>
+                  </div>
+                )}
+                {teacher.portfolio && (
+                  <div className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                    <dt className="text-sm font-medium text-gray-500">
+                      Portfolio
+                    </dt>
+                    <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                      {teacher.portfolio}
+                    </dd>
+                  </div>
+                )}
+                {teacher.personalwebsite && (
+                  <div className="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                    <dt className="text-sm font-medium text-gray-500">
+                      Personal Website
+                    </dt>
+                    <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                      {teacher.personalwebsite}
+                    </dd>
+                  </div>
+                )}
+                {teacher.cv && (
+                  <div className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                    <dt className="text-sm font-medium text-gray-500">
+                      Attachments
+                    </dt>
+                    <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                      <ul
+                        role="list"
+                        className="border border-gray-200 rounded-md divide-y divide-gray-200"
+                      >
+                        <li className="pl-3 pr-4 py-3 flex items-center justify-between text-sm">
+                          <div className="w-0 flex-1 flex items-center">
+                            <svg
+                              className="flex-shrink-0 h-5 w-5 text-gray-400"
+                              xmlns="http://www.w3.org/2000/svg"
+                              viewBox="0 0 20 20"
+                              fill="currentColor"
+                              aria-hidden="true"
                             >
-                              Apply under this mentor
-                            </button>
+                              <path
+                                fillRule="evenodd"
+                                d="M8 4a3 3 0 00-3 3v4a5 5 0 0010 0V7a1 1 0 112 0v4a7 7 0 11-14 0V7a5 5 0 0110 0v4a3 3 0 11-6 0V7a1 1 0 012 0v4a1 1 0 102 0V7a3 3 0 00-3-3z"
+                                clipRule="evenodd"
+                              />
+                            </svg>
+                            <span className="ml-2 flex-1 w-0 truncate">
+                              {" "}
+                              cv.pdf{" "}
+                            </span>
                           </div>
-                        )}
-                      </li>
-                      <li className="pl-3 pr-4 py-3 flex items-center justify-between text-sm">
-                        {user && user.loginemail == teacher.email && (
-                          <div className="p-2 w-full">
-                            <Link href={`/updateprofile/${teacher.email}`}>
-                              <a>
-                                <button className="w-full border-2 text-white bg-indigo-500 rounded-md p-2 hover:bg-indigo-600">
-                                  Update Profile
-                                </button>
-                              </a>
-                            </Link>
+                          <div className="ml-4 flex-shrink-0">
+                            <a
+                              href={teacher.cv}
+                              className="font-medium text-indigo-600 hover:text-indigo-500"
+                            >
+                              {" "}
+                              View{" "}
+                            </a>
                           </div>
+                        </li>
+                      </ul>
+                    </dd>
+                  </div>
+                )}
+                {user && (
+                  <div className="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                    <dt className="text-sm font-medium text-gray-500">
+                      Actions
+                    </dt>
+                    <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                      <ul
+                        role="list"
+                        className="border border-gray-200 rounded-md divide-y divide-gray-200"
+                      >
+                        {user.loginemail != teacher.email && (
+                          <li className="pl-3 pr-4 py-3 flex items-center justify-between text-sm">
+                            <div className="p-2 w-full">
+                              <button
+                                onClick={updateDocument}
+                                className="w-full border-2 text-white bg-indigo-500 rounded-md p-2 hover:bg-indigo-600"
+                              >
+                                Apply under this mentor
+                              </button>
+                            </div>
+                          </li>
                         )}
-                      </li>
-                    </ul>
-                  </dd>
-                </div>
+                        {user.loginemail == teacher.email && (
+                          <li className="pl-3 pr-4 py-3 flex items-center justify-between text-sm">
+                            <div className="p-2 w-full">
+                              <Link href={`/updateprofile/${teacher.email}`}>
+                                <a>
+                                  <button className="w-full border-2 text-white bg-indigo-500 rounded-md p-2 hover:bg-indigo-600">
+                                    Update Profile
+                                  </button>
+                                </a>
+                              </Link>
+                            </div>
+                          </li>
+                        )}
+                      </ul>
+                    </dd>
+                  </div>
+                )}
               </dl>
             </div>
           </div>
