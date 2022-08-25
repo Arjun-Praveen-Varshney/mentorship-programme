89b5857da319c6f67f93639460ad96d6d67befb7
Maverick-1423 <99124051+Maverick-1423@users.noreply.github.com>
2022-08-26 00:49:40 +0530

    updated menteeprofile page

--- pages/menteeprofiles/[slug].js
+++ pages/menteeprofiles/[slug].js
@@ -136,57 +136,64 @@ const Slug = ({ onementee }) => {
                   </div>
                 )}
 
-                <div className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-                  <dt className="text-sm font-medium text-gray-500">
-                    Attachments
-                  </dt>
-                  <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
-                    <ul
-                      role="list"
-                      className="border border-gray-200 rounded-md divide-y divide-gray-200"
-                    >
-                      <li className="pl-3 pr-4 py-3 flex items-center justify-between text-sm">
-                        <div className="w-0 flex-1 flex items-center">
-                          <svg
-                            className="flex-shrink-0 h-5 w-5 text-gray-400"
-                            xmlns="http://www.w3.org/2000/svg"
-                            viewBox="0 0 20 20"
-                            fill="currentColor"
-                            aria-hidden="true"
-                          >
-                            <path
-                              fillRule="evenodd"
-                              d="M8 4a3 3 0 00-3 3v4a5 5 0 0010 0V7a1 1 0 112 0v4a7 7 0 11-14 0V7a5 5 0 0110 0v4a3 3 0 11-6 0V7a1 1 0 012 0v4a1 1 0 102 0V7a3 3 0 00-3-3z"
-                              clipRule="evenodd"
-                            />
-                          </svg>
-                          <span className="ml-2 flex-1 w-0 truncate">
-                            {" "}
-                            {student.cv}{" "}
-                          </span>
-                        </div>
-                        <div className="ml-4 flex-shrink-0">
-                          <a
-                            href="#"
-                            className="font-medium text-indigo-600 hover:text-indigo-500"
-                          >
-                            {" "}
-                            Download{" "}
-                          </a>
-                        </div>
-                      </li>
-                    </ul>
-                  </dd>
-                </div>
-                <div className="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-                  <dt className="text-sm font-medium text-gray-500">Actions</dt>
-                  <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
-                    <ul
-                      role="list"
-                      className="border border-gray-200 rounded-md divide-y divide-gray-200"
-                    >
-                      <li className="pl-3 pr-4 py-3 flex items-center justify-between text-sm">
-                        {user && user.loginemail == student.email && (
+                {student.cv != "" && (
+                  <div className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                    <dt className="text-sm font-medium text-gray-500">
+                      Attachments
+                    </dt>
+                    <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                      <ul
+                        role="list"
+                        className="border border-gray-200 rounded-md divide-y divide-gray-200"
+                      >
+                        <li className="pl-3 pr-4 py-3 flex items-center justify-between text-sm">
+                          <div className="w-0 flex-1 flex items-center">
+                            <svg
+                              className="flex-shrink-0 h-5 w-5 text-gray-400"
+                              xmlns="http://www.w3.org/2000/svg"
+                              viewBox="0 0 20 20"
+                              fill="currentColor"
+                              aria-hidden="true"
+                            >
+                              <path
+                                fillRule="evenodd"
+                                d="M8 4a3 3 0 00-3 3v4a5 5 0 0010 0V7a1 1 0 112 0v4a7 7 0 11-14 0V7a5 5 0 0110 0v4a3 3 0 11-6 0V7a1 1 0 012 0v4a1 1 0 102 0V7a3 3 0 00-3-3z"
+                                clipRule="evenodd"
+                              />
+                            </svg>
+                            <span className="ml-2 flex-1 w-0 truncate">
+                              {" "}
+                              cv.pdf{" "}
+                            </span>
+                          </div>
+
+                          <div className="ml-4 flex-shrink-0">
+                            <a
+                              rel="noreferrer"
+                              target="_blank"
+                              href={student.cv}
+                              className="font-medium text-indigo-600 hover:text-indigo-500"
+                            >
+                              {" "}
+                              View{" "}
+                            </a>
+                          </div>
+                        </li>
+                      </ul>
+                    </dd>
+                  </div>
+                )}
+                {user && user.loginemail == student.email && (
+                  <div className="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                    <dt className="text-sm font-medium text-gray-500">
+                      Actions
+                    </dt>
+                    <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                      <ul
+                        role="list"
+                        className="border border-gray-200 rounded-md divide-y divide-gray-200"
+                      >
+                        <li className="pl-3 pr-4 py-3 flex items-center justify-between text-sm">
                           <div className="p-2 w-full">
                             <Link href={`/updateprofile/${student.email}`}>
                               <a>
@@ -196,11 +203,11 @@ const Slug = ({ onementee }) => {
                               </a>
                             </Link>
                           </div>
-                        )}
-                      </li>
-                    </ul>
-                  </dd>
-                </div>
+                        </li>
+                      </ul>
+                    </dd>
+                  </div>
+                )}
               </dl>
             </div>
           </div>
